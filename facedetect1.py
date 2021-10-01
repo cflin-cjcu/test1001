f964faf413675a4d489169a3737abe6935729b2c
cflin <cflin@mail.cjcu.edu.tw>
2021-10-01 18:22:39 +0800

    first commit

---
+++ facedetect1.py
@@ -0,0 +1,21 @@
+from FaceDetectionModule import FaceDetector
+import cv2
+
+cap = cv2.VideoCapture(0)
+detector = FaceDetector()
+
+while True:
+    # success, img = cap.read()
+    img = cv2.imread('./image/test2.jpg')
+    img, bboxs = detector.findFaces(img)
+
+    if bboxs:
+        # bboxInfo - "id","bbox","score","center"
+        center = bboxs[0]["center"]
+        cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
---
+++ test.py
@@ -0,0 +1,14 @@
+import cv2
+
+print("AAA")
+
+cv2.imread()
+
+# Import only if not previously imported
+VariableName = cv2.imread("Address", flag)
+
+# Import only if not previously imported
+contours, hierarchy = cv2.findContours(binaryImg, cv2.retrievalMode, cv2.approximation method)
+# Colors of contours is kept green by default
+cv2.drawContours(Image, contours, contourIdx, (0, 255, 0), thickness)
+# contourIdx = -1 if you want to draw all contours
